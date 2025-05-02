a86a6dcdef5ed75ef18d51bf3f1ac4c3f45fe008
MF-Dust <MF-Dust@users.noreply.github.com>
2025-05-02 13:10:59 +0000

    deploy: 0ed433beb4fbd7378f448a0d6a8b51e44e823771

---
+++ assets/ssrBoot-m1Fxwm88.js
@@ -0,0 +1,1 @@
+import{U as t,$ as e,k as s,a_ as n}from"./index-CXuoiuhD.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
